84065eb7634ec1a3eefbc365b0345871a9c7df74
teerawat prarom <teewat@yahoo.com>
2021-08-02 14:11:28 +1000

    challage1-3 done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
-console.log('%c HI', 'color: firebrick')
+console.log("%c HI", "color: firebrick");
+
+const fetchService = (url, fn) => {
+  fetch(url)
+    .then((resp) => resp.json())
+    .then((json) => {
+      fn(json.message);
+      //console.log(json.message);
+    });
+};
+
+const displayImages = (dogs) => {
+  const dogImages = document.getElementById("dog-image-container");
+  dogs.forEach((dog) => {
+    const img = document.createElement("img");
+    img.setAttribute("src", `${dog}`);
+    img.setAttribute("height", 100);
+    img.setAttribute("width", 100);
+    dogImages.appendChild(img);
+  });
+};
+
+const displayBreeds = (dogs) => {
+  const breeds = Object.keys(dogs);
+  const dogBreeds = document.getElementById("dog-breeds");
+  breeds.forEach((breed) => {
+    const li = document.createElement("li");
+    li.setAttribute("id",`${breed}`);
+    li.innerText = breed;
+    dogBreeds.appendChild(li);
+    const liI = document.getElementById(breed);
+    liI.addEventListener("click", (e) => {
+        liI.style.color = "#ff0000";
+    });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+  fetchService(imgUrl, displayImages);
+  const breedUrl = "https://dog.ceo/api/breeds/list/all";
+  fetchService(breedUrl, displayBreeds);
+});
